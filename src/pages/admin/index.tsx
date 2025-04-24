09fcf2eec251f926660ba5fa326984ba158a337e
restu adil <restuadil09@gmail.com>
2025-04-24 13:16:10 +0700

    fixing cn fucntion

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -5,7 +5,6 @@ export default function Page() {
   return (
     <div>
       <div>Admin page</div>
-      <h1>Welcome {session?.user?.username}</h1>
       <p>Email: {session?.user?.email}</p>
     </div>
   );
